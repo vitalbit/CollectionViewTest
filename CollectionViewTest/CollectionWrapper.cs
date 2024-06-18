2777acde53fcea6eb9eb7cebe63175272331af53
Vitaly Ponteley <vitaly.ponteley@innowise-group.com>
2024-06-18 09:01:05 +0200

    Add project files.

---
+++ CollectionViewTest/CollectionWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionViewTest;
+public class CollectionWrapper : INotifyPropertyChanged
+{
+    private ObservableCollection<string> _toDisplay = [];
+
+    public ObservableCollection<string> ToDisplay
+    {
+        get { return _toDisplay; }
+        set 
+        { 
+            _toDisplay = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        if (propertyName is not null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ CollectionViewTest/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectionViewTest;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    private CollectionWrapper _items = new();
+
+    public CollectionWrapper Items
+    {
+        get => _items;
+        set
+        {
+            _items = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        _ = Task.Run(() =>
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Items.ToDisplay.Add("Test item " + i.ToString());
+            }
+        });
+    }
+}
+
